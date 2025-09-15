a374b68367555e1da44d636ba0fd90f1df40f572
Michael Muhov <mcmbrest@gmail.com>
2025-09-15 19:32:27 +0200

    [Basket] Add product to basket

--- app/services/basket.rb
+++ app/services/basket.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Basket
+  class Error < StandardError; end
+
   def initialize(delivery_rules:, offers:)
     @delivery_rules = delivery_rules
     @offers = offers
@@ -11,7 +13,10 @@ class Basket
   attr_reader :delivery_rules, :offers, :products
 
   def add(code)
-    # Add product to basket
+    product = Product.find_by(code:)
+    raise Error, "Product not found" unless product
+
+    @products << product.code
   end
 
   def total
